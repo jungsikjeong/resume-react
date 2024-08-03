a11272ea473da39603c0eb47d311cf48052ce2c1
jungsikjeong <wndtlr1024@gmail.com>
2024-08-04 01:23:14 +0900

    Chore: yarn berry pnp모드적용

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import activity from '../../assets/json/activity.json';
-import author from '../../assets/json/author.json';
-import authorDetails from '../../assets/json/authorDetails.json';
-import careerModalContents from '../../assets/json/career-modal.json';
-import career from '../../assets/json/career.json';
-import skills from '../../assets/json/skills.json';
+import { useState } from 'react'
+import styled from 'styled-components'
+import activity from '../../assets/json/activity.json'
+import author from '../../assets/json/author.json'
+import authorDetails from '../../assets/json/authorDetails.json'
+import careerModalContents from '../../assets/json/career-modal.json'
+import career from '../../assets/json/career.json'
+import skills from '../../assets/json/skills.json'
 
-import DarkModeButton from '../../components/dark-mode-button/dark-mode-button';
-import Intro from '../../components/intro/intro';
-import Section from '../../components/section';
+import DarkModeButton from '../../components/dark-mode-button/dark-mode-button'
+import Intro from '../../components/intro/intro'
+import Section from '../../components/section'
 
 const Container = styled.div`
   position: relative;
   padding-bottom: 10rem;
-`;
+`
 
 const Box = styled.div`
   display: grid;
@@ -24,17 +24,17 @@ const Box = styled.div`
   @media (max-width: 480px) {
     grid-template-columns: repeat(1, 1fr);
   }
-`;
+`
 
 const HomePage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <Container>
       <DarkModeButton />
       <Intro />
 
       <Section
-        name='someone'
+        name="someone"
         title={'🙆‍♂️ 저는 이런사람이에요!'}
         contents={author?.contents}
         style={{ fontSize: '1.4rem' }}
@@ -42,7 +42,7 @@ const HomePage = () => {
 
       <Box>
         <Section
-          name='career'
+          name="career"
           title={'🎥 전문성 쌓아나가는중..'}
           contents={career?.contents}
           careerModalContents={careerModalContents}
@@ -51,23 +51,23 @@ const HomePage = () => {
         />
 
         <Section
-          name='blog'
+          name="blog"
           title={'😸 제가 더 궁금하시다면!'}
           contents={authorDetails?.contents}
         />
       </Box>
 
-      <Section name='skills' title={'💻 Skills'} contents={skills?.contents} />
+      <Section name="skills" title={'💻 Skills'} contents={skills?.contents} />
 
-      <Section name='portfolio' title={'🎨 Portfolio'} contents={[]} />
+      <Section name="portfolio" title={'🎨 Portfolio'} contents={[]} />
 
       <Section
-        name='activity'
+        name="activity"
         title={'🏃‍♀️ Activity'}
         contents={activity.contents}
       />
     </Container>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
